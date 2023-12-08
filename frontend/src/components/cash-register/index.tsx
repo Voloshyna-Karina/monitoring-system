13337148738007164e8d0545e7ea1a0f5955ff5e
Voloshyna Karina <voloshynakarina29@gmail.com>
2023-12-08 19:10:05 +0200

    delite

--- frontend/src/components/cash-register/index.tsx
+++ frontend/src/components/cash-register/index.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
+import cashier from '../../assets/images/cash-register/cashier.png';
+import { Box, Typography, Modal, TextField, Button } from "@mui/material";
+import { ListItem, List } from "@mui/material";
+import { Fab } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 const CashRegisterComponent = () => {
-    return (
-        <div>
-            <h1>CashRegisterComponent</h1>
-        </div>
-    );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddNewCashRegister = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 3,
+    boxShadow: 30,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  return (
+    <Box className="CashRegisterComponentBlock">
+      <Typography className="HeaderText" variant="h6" component="h2">Каси</Typography>
+      <Box className="CashRegisterContainer">
+        <List className="CashRegisterList">
+          <ListItem className="CashRegisterListItem">
+            <img src={cashier} alt="cash register" width={150} height={150} />
+          </ListItem>
+        </List>
+        <Fab id="AddNewCashRegisterButton" size="small" color="primary" aria-label="add" onClick={handleAddNewCashRegister}>
+          <AddIcon />
+        </Fab>
+      </Box>
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <Box sx={modalStyle}>
+          {/* Здесь вы можете разместить содержимое вашего всплывающего окна */}
+          <Typography variant="h4" fontFamily='Iter' textAlign='center' marginBottom={2}>Додати нову касу</Typography>
+          <TextField fullWidth={true} margin='normal' id="#" label="Номер каси" variant="outlined" placeholder="Введіть номер каси" />
+          <TextField fullWidth={true} margin='normal' id="#" label="Адреса" variant="outlined" placeholder="Введіть адресу" />
+          <Button id="ADDDDD" sx={{marginTop: 2, width: '30%', marginBottom: 1}} variant="contained">Додати</Button>
+        </Box>
+      </Modal>
+    </Box>
+  );
 };
 
 export default CashRegisterComponent;
--- frontend/src/components/product/index.tsx
+++ frontend/src/components/product/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Box} from "@mui/material"
 
 const ProductComponent = () => {
     return (
-        <div>
-            <h1>ProductComponent</h1>
-        </div>
+        <Box>
+            <div>
+                <h1>ProductContent</h1>
+            </div>
+        </Box>
     );
 };
 
